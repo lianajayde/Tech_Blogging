c9205e4e7acd04eeed00b39c343e89f761944f89
lianajayde <lianajaydee@gmail.com>
2023-04-13 18:34:32 -0500

    handlebars

--- public/js/new.js
+++ public/js/new.js
@@ -1,18 +1,17 @@
 const newFormHandler = async function(event) {
-    event.preventDefault();
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('textarea[name="post-content"]').value;
-  
-    await fetch(`/api/post`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        post_content
-      }),
-      headers: {'Content-Type': 'application/json' }
-    });
-  
-     document.location.replace('/dashboard');
-  };
-  
-  document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
+  event.preventDefault();
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document.querySelector('textarea[name="post-content"]').value;
+
+  await fetch(`/api/post`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      post_content
+    }),
+    headers: {'Content-Type': 'application/json'}
+  });
+   document.location.replace('/dashboard');
+};
+
+document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
