fbf288073a9e6bba1c2a453660937f4379145ee0
lianajayde <lianajaydee@gmail.com>
2023-04-13 17:50:25 -0500

    more js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,6 +1,5 @@
 const commentFormHandler = async function(event) {
     event.preventDefault();
-  
     const postId = document.querySelector('input[name="-post-id"]').value;
     const body = document.querySelector('textarea [name="comment-body"]').value;
     
